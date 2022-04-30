79abebbdfe5e255d58dda71c609e1730c2040ec8
Patrick Bittman <pat.bittman@gmail.com>
2022-04-30 12:01:44 -0400

    writing individual pixels cleanly

--- obj/PixelArray.py
+++ obj/PixelArray.py
@@ -4,11 +4,20 @@ from obj.Pixel import Pixel
 
 class PixelArray():
 
-    def __init__(self, rows, cols):
-        self.rows = int(rows)
-        self.cols = int(cols)
-        self.arr = [[Pixel(0, 0, 0) for i in range(self.cols)] for j in range(self.rows)]
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+        self.arr = [[Pixel(0, 0, 0) for i in range(self.y)] for j in range(self.x)]
 
     def show(self):
-        img = Image.new(mode="RGB", size=(self.cols, self.rows))
+        img = Image.new(mode="RGB", size=(self.x, self.y))
+        for x in range(self.x):
+            for y in range(self.y):
+                px = self.arr[x][y]
+                xy = (x, y)
+                rgu = (px.r, px.g, px.u)
+                img.putpixel(xy, rgu)
         img.show()
+
+    def setp(self, x, y, r, g, u):
+        self.arr[x][y] = Pixel(r, g, u)
--- soul.py
+++ soul.py
@@ -2,5 +2,8 @@ import util.const as const
 
 from obj.PixelArray import PixelArray
 
-parr = PixelArray(1000, 1000 * const.GOLDEN)
+parr = PixelArray(1000 * const.GOLDEN, 1000)
+for x in range(parr.x):
+    for y in range(parr.y):
+        parr.setp(x, y, x%255, y%255, (x+y)%255)
 parr.show()
